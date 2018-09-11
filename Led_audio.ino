639088e6ea3b1e76d65c1f7d641c64ee0068e99c
Grensom <36168234+Grensom@users.noreply.github.com>
2018-09-10 22:30:41 -0400

    Add files via upload

---
+++ Led_audio.ino
@@ -0,0 +1,49 @@
+#include <FastLED.h>
+
+#define num_leds 60
+#define pin 6
+#define brightness 50
+#define sensitivity 5
+
+CRGB leds[num_leds];
+
+void setup() {
+  FastLED.addLeds<WS2811, pin, GRB>(leds, num_leds);
+  FastLED.setBrightness(brightness);
+}
+
+void loop() {
+  int x = analogRead(0);
+  x = x * sensitivity;
+    if (x < 71) {
+      leds[30] = CRGB(255, 0, 0);
+    }
+    else if (x > 71 && x <= 142) {
+      leds[30] = CRGB(255, 154, 0);
+    }
+    else if (x > 142 && x <= 213) {
+      leds[30] = CRGB(255, 255, 0);
+    }
+    else if (x > 213 && x <= 284) {
+      leds[30] = CRGB(0, 255, 0);
+    }
+    else if (x > 284 && x <= 355) {
+      leds[30] = CRGB(0, 0, 255);
+    }
+    else if (x > 355 && x <= 426) {
+      leds[30] = CRGB(150, 102, 255);
+    }
+    else {
+      leds[30] = CRGB(255, 0, 255);
+    }
+ 
+  FastLED.show();
+  delay(10);
+  for (int z = 60; z > 30; z--) {
+    leds[z] = leds[z - 1];
+  }
+  for (int z = 0; z < 30; z++) {
+    leds[z] = leds[z + 1];
+  }
+}
+
---
+++ Led_audio_cycle_bright_sens_display.ino
@@ -0,0 +1,138 @@
+#include <FastLED.h>
+#include <LiquidCrystal.h>
+
+#define num_leds 60
+#define pin 6
+
+CRGB leds[num_leds];
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  Serial.begin(9600);
+  FastLED.addLeds<WS2811, pin, GRB>(leds, num_leds);
+  lcd.begin(16, 2); //Initialize the 16x2 LCD
+  lcd.clear();  //Clear any old data displayed on the LCD
+}
+
+void loop() {
+  int brightness = (analogRead(2)*0.24);
+  int sensitivity = (analogRead(3)*0.009);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Brightness:  ");
+  lcd.print(round(analogRead(2)*0.09775171));
+  lcd.print("%");
+  lcd.setCursor(0, 1);
+  lcd.print("Sensitivity: ");
+  lcd.print(round(analogRead(3)*0.09775171));
+  lcd.print("%");
+  int x = analogRead(0);
+  x = x * sensitivity;
+  int y = analogRead(1);
+  int d;
+  //  Serial.println(y);
+  int c = 0;
+  FastLED.setBrightness(brightness);
+  if (y < 100) {
+    if (x < 71) {
+      leds[30] = CRGB(255, 0, 0);
+    }
+    else if (x > 71 && x <= 142) {
+      leds[30] = CRGB(255, 154, 0);
+    }
+    else if (x > 142 && x <= 213) {
+      leds[30] = CRGB(255, 255, 0);
+    }
+    else if (x > 213 && x <= 284) {
+      leds[30] = CRGB(0, 255, 0);
+    }
+    else if (x > 284 && x <= 355) {
+      leds[30] = CRGB(0, 0, 255);
+    }
+    else if (x > 355 && x <= 426) {
+      leds[30] = CRGB(150, 102, 255);
+    }
+    else {
+      leds[30] = CRGB(255, 0, 255);
+    }
+  }
+  else if (y > 100 && y <= 1000) {
+    if (y >= 100 && y < 250) { //2nd up
+      c = ((y - 100) * (255 / 150));
+      leds[30] = CRGB(255, c, 0);
+    }
+    else if (y >= 250 && y < 400) { //1st down
+      c = ((y - 400) * (-255 / 150));
+      leds[30] = CRGB(c, 255, 0);
+    }
+    else if (y >= 400 && y < 550) { //3rd up
+      c = ((y - 400) * (255 / 150));
+      leds[30] = CRGB(0, 255, c);
+    }
+    else if (y >= 550 && y < 700) {//2nd down
+      c = ((y - 700) * (-255 / 150));
+      leds[30] = CRGB(0, c, 255);
+    }
+    else if (y >= 700 && y < 850) {//1st up
+      c = ((y - 700) * (255 / 150));
+      leds[30] = CRGB(c, 0, 255);
+    }
+    else if (y >= 850 && y < 1000) {//3rd down
+      c = ((y - 1000) * (-255 / 150));
+      leds[30] = CRGB(255, 0, c);
+    }
+    else {
+      leds[30] = CRGB(255, 0, 0);
+    }
+  }
+  else {
+    int a = y;
+    int d;
+    while (y > 1000) {
+      y = analogRead(1);
+      if (a >= 100 && a < 250) { //2nd up
+        d = ((a - 100) * (255 / 150));
+        leds[30] = CRGB(255, d, 0);
+      }
+      else if (a >= 250 && a < 400) { //1st down
+        d = ((a - 400) * (-255 / 150));
+        leds[30] = CRGB(d, 255, 0);
+      }
+      else if (a >= 400 && a < 550) { //3rd up
+        d = ((a - 400) * (255 / 150));
+        leds[30] = CRGB(0, 255, d);
+      }
+      else if (a >= 550 && a < 700) {//2nd down
+        d = ((a - 700) * (-255 / 150));
+        leds[30] = CRGB(0, d, 255);
+      }
+      else if (a >= 700 && a < 850) {//1st up
+        d = ((a - 700) * (255 / 150));
+        leds[30] = CRGB(d, 0, 255);
+      }
+      else if (a >= 850 && a < 1000) {//3rd down
+        d = ((a - 1000) * (-255 / 150));
+        leds[30] = CRGB(255, 0, d);
+      }
+      else {
+        leds[30] = CRGB(255, 0, 0);
+      }
+      FastLED.show();
+      delay(60);
+      fill_solid( leds, num_leds, leds[30]);
+      a = a + 1;
+      if (a > 1023){
+        a = 0;
+      }
+    }
+  }
+  FastLED.show();
+  delay(10);
+  for (int z = 60; z > 30; z--) {
+    leds[z] = leds[z - 1];
+  }
+  for (int z = 0; z < 30; z++) {
+    leds[z] = leds[z + 1];
+  }
+}
+
