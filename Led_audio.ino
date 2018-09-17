25bab34b5d519e53b896be96ad027b6ee3e93615
Grensom <36168234+Grensom@users.noreply.github.com>
2018-09-17 18:48:54 -0400

    Update Led_audio.ino

--- Led_audio.ino
+++ Led_audio.ino
@@ -1,9 +1,14 @@
+/*
+Grensom LED-Audio Spectrum
+Only thing that needs to be adjusted is the "num_leds", "brightness", and "sensitivity" in this sketch to how many LEDs you are using
+*/
+
 #include <FastLED.h>
 
 #define num_leds 60
 #define pin 6
-#define brightness 50
-#define sensitivity 5
+#define brightness 50     // can set 0-255
+#define sensitivity 5     // can set 0-10, recommended at 5
 
 CRGB leds[num_leds];
 
@@ -16,25 +21,25 @@ void loop() {
   int x = analogRead(0);
   x = x * sensitivity;
     if (x < 71) {
-      leds[30] = CRGB(255, 0, 0);
+      leds[(num_leds/2)] = CRGB(255, 0, 0);
     }
     else if (x > 71 && x <= 142) {
-      leds[30] = CRGB(255, 154, 0);
+      leds[(num_leds/2)] = CRGB(255, 154, 0);
     }
     else if (x > 142 && x <= 213) {
-      leds[30] = CRGB(255, 255, 0);
+      leds[(num_leds/2)] = CRGB(255, 255, 0);
     }
     else if (x > 213 && x <= 284) {
-      leds[30] = CRGB(0, 255, 0);
+      leds[(num_leds/2)] = CRGB(0, 255, 0);
     }
     else if (x > 284 && x <= 355) {
-      leds[30] = CRGB(0, 0, 255);
+      leds[(num_leds/2)] = CRGB(0, 0, 255);
     }
     else if (x > 355 && x <= 426) {
-      leds[30] = CRGB(150, 102, 255);
+      leds[(num_leds/2)] = CRGB(150, 102, 255);
     }
     else {
-      leds[30] = CRGB(255, 0, 255);
+      leds[(num_leds/2)] = CRGB(255, 0, 255);
     }
  
   FastLED.show();
